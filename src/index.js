2405e6ff1934e53159fd1c18896dfeb9626223e2
Ivan <kasiama@yandex.ru>
2018-09-11 11:42:37 +0300

    addingAlg

--- src/index.js
+++ src/index.js
@@ -3,5 +3,27 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	let result = 0;
+  	let used = [];
+  	for(let i = 0;i+3<=preferences.length;i+=1){
+  		let first = preferences[i];
+  		if(!used[first] && first!=preferences[first-1])
+  		{
+  			let second = preferences[first-1];
+  			if (!used[second] && second != preferences[second-1]){
+  				let third = preferences[second-1];
+  				if(!used[third] && third != preferences[third-1] && third == i+1 ){
+  					result++;
+  					used[first]=1;
+  					used[second]=1;
+  					used[third]=1;
+  				}
+  			}
+
+  		}
+  			
+  	}
+  		
+  		
+return result;
 };
